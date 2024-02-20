5a9e3b59cbf9025bc0fe63df32d7d9efd81f5a79
Chandan Kumar Saw <chandan1951c@gmail.com>
2024-02-20 20:49:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let imgbox = document.getElementById("imgbox");
 let qrimg = document.getElementById("qrimg");
 let qrtext = document.getElementById("qrtext");
+let error = document.getElementById("error");
 
 function generateQr()
 {
     if(qrtext.value.length > 0)
     {
+        imgbox.style.display = "block"
+        error.style.display = "none";
+        // Generate QR Code
         qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        
         imgbox.classList.add("show-img");
+        
     }
     else{
-        qrtext.classList.add("error");
-        setTimeout(()=>{
-            qrtext.classList.remove('error');
-        },1000);
+        imgbox.style.display = "none"
+        error.style.display = "block";
+        // alert("Please enter some text!");
     }
     
 }
